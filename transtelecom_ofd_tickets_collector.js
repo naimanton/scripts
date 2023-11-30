c4115b05143bdd3e3d0b2b8330ce6be0479c97ff
naimanton <93698269+naimanton@users.noreply.github.com>
2023-11-30 13:16:47 +0600

    actual on 23-11-30

--- transtelecom_ofd_tickets_collector.js
+++ transtelecom_ofd_tickets_collector.js
@@ -1,32 +1,35 @@
-void function ttcofdtc() {
-const cfg = {
-	logTab: '--------',
-	pauses: {
-		shift: 5000,
-		ticket: 2000,
+const ttcofdtc = { 
+	cfg: {
+		logTab: '--------',
+		pauses: {
+			shift: 2000,
+			ticket: 5000,
+		},
+		sec_ch_ua_header: "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
+		ermsg: {
+			noStartDate: '#start_date === null',
+			noEndDate: '#end_date === null',
+			noTicketsButtons: 'ticketsButtons.length <= 0',
+			noShiftID: 'no data-shift-id',
+			ticketsListDataIsNotArray: 'ticketsList.data is not Array',
+			noShiftNumber: 'shift-number is null',
+			noTicketID: 'ticket.id is undefined',
+		},
 	},
-	sec_ch_ua_header: "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
-	ermsg: {
-		noStartDate: '#start_date === null',
-		noEndDate: '#end_date === null',
-		noTicketsButtons: 'ticketsButtons.length <= 0',
-		noShiftID: 'no data-shift-id',
-		ticketsListDataIsNotArray: 'ticketsList.data is not Array',
-		noShiftNumber: 'shift-number is null',
-		noTicketID: 'ticket.id is undefined',
+	state: {
+		dates: null,
+		cashID: null,
+		referrer: null,
+		manualThrow: false,
+	},
+	result : {
+		cashID: null,
+		datesRange: null,
+		unknownTypeTickets: {},
+		shifts: {},
 	},
 };
-const ttcofdtcResult = {
-	cashID: null,
-	datesRange: null,
-	shifts: {},
-};
-const state = {
-	dates: null,
-	cashID: null,
-	referrer: null,
-};
-
+void function ttcofdtcFunc() {
 const validation = {
 	assert(boolean, message='assertion') {
 		if (boolean) return;
@@ -40,16 +43,16 @@ const validation = {
 	},	
 };
 const dataWorker = {
-	getDates() {
-		const startDateInput = document.querySelector('#start_date');
-		const endDateInput = document.querySelector('#end_date');
+	getDates(ticketsButton) {
+		const startDate = ticketsButton.getAttribute('data-start-date');
+		const endDate = ticketsButton.getAttribute('data-end-date');
 
-		validation.assert(startDateInput !== null, cfg.ermsg.noStartDate);
-		validation.assert(endDateInput !== null, cfg.ermsg.noEndDate);
+		validation.assert(startDate !== null, ttcofdtc.cfg.ermsg.noStartDate);
+		validation.assert(endDate !== null, ttcofdtc.cfg.ermsg.noEndDate);
 
 		return {
-			start: startDateInput.getAttribute('value'),
-			end: endDateInput.getAttribute('value'),
+			start: startDate,
+			end:  endDate,
 		};
 	},
 	getConvertedTicket(ticket) {
@@ -123,68 +126,78 @@ const manager = {
 		});
 	},
 	async work() {
-		manager.log(cfg.logTab, 'Инициализация...');
-		state.dates = dataWorker.getDates();
-		ttcofdtcResult.datesRange = state.dates;
-		manager.log(cfg.logTab, cfg.logTab, 'Даты извлечены:');
-		manager.log(cfg.logTab, cfg.logTab, state.dates);
-		state.referrer = document.location.href;
+		manager.log('Инициализация...');
+		ttcofdtc.state.referrer = document.location.href;
 
 		const ticketsButtons = [...document.querySelectorAll('button[data-shift-id]')];
-		validation.assert(ticketsButtons.length > 0, cfg.ermsg.noTicketsButtons);
-		manager.log(cfg.logTab, cfg.logTab, 'Смены собраны: ', ticketsButtons.length);
-		state.cashID = ticketsButtons[0].getAttribute('data-cash-id');
-		ttcofdtcResult.cashID = state.cashID;
+		validation.assert(ticketsButtons.length > 0, ttcofdtc.cfg.ermsg.noTicketsButtons);
+		manager.log(ttcofdtc.cfg.logTab, 'Смены собраны: ', ticketsButtons.length);
+		ttcofdtc.state.dates = dataWorker.getDates(ticketsButtons[0]);
+		ttcofdtc.result.datesRange = ttcofdtc.state.dates;
+		manager.log(ttcofdtc.cfg.logTab, 'Даты извлечены:');
+		manager.log(ttcofdtc.cfg.logTab, ttcofdtc.state.dates);
+		ttcofdtc.state.cashID = ticketsButtons[0].getAttribute('data-cash-id');
+		ttcofdtc.result.cashID = ttcofdtc.state.cashID;
 		await manager.handleTicketsCollecting(ticketsButtons);
-		console.log(JSON.stringify(ttcofdtcResult));
+		console.log(JSON.stringify(ttcofdtc.result));
 	},
 	async handleTicketsCollecting(ticketsButtons) {
-		manager.log(cfg.logTab, 'Cбор чеков...');
+		manager.log('Cбор чеков...');
 		for (let ticketsButtonIndex = 0; ticketsButtonIndex < ticketsButtons.length; ticketsButtonIndex++) {
-			manager.log(cfg.logTab, 'Смена ', ticketsButtonIndex+1, '/', ticketsButtons.length);
+			manager.log('Смена ', ticketsButtonIndex+1, '/', ticketsButtons.length);
 			await manager.handleTicketsButton(ticketsButtons[ticketsButtonIndex]);
 		}
 	},
 	async handleTicketsButton(ticketsButton) {
 		const shiftID = ticketsButton.getAttribute('data-shift-id');
-		validation.assert(shiftID !== null, cfg.ermsg.noShiftID);
+		validation.assert(shiftID !== null, ttcofdtc.cfg.ermsg.noShiftID);
 		
 		const ticketsListResponse = await fetcher.ticketsList(
-			state.cashID, shiftID,
-			state.dates.start, state.dates.end,
-			cfg.sec_ch_ua_header, state.referrer 
+			ttcofdtc.state.cashID, shiftID,
+			ttcofdtc.state.dates.start, ttcofdtc.state.dates.end,
+			ttcofdtc.cfg.sec_ch_ua_header, ttcofdtc.state.referrer 
 		);
 		const ticketsList = await ticketsListResponse.json();
-		validation.assert(Array.isArray(ticketsList.data), cfg.ermsg.ticketsListDataIsNotArray);
-		manager.log(cfg.logTab, cfg.logTab, 'Получено сырых чеков: ', ticketsList.data.length);
+		validation.assert(Array.isArray(ticketsList.data), ttcofdtc.cfg.ermsg.ticketsListDataIsNotArray);
+		manager.log(ttcofdtc.cfg.logTab, 'Получено сырых чеков: ', ticketsList.data.length);
 		const filteredTicketsArray = ticketsList.data.filter(
 			item => item.operation_type === 'OPERATION_SELL'
 		);
-		manager.log(cfg.logTab, cfg.logTab, 'Отфильтровано сырых чеков: ', filteredTicketsArray.length);
+		manager.log(ttcofdtc.cfg.logTab, 'Отфильтровано сырых чеков: ', filteredTicketsArray.length);
 		const shiftNumber = ticketsButton.getAttribute('shift-number');
-		validation.assert(shiftNumber !== null, cfg.ermsg.noShiftNumber);
+		validation.assert(shiftNumber !== null, ttcofdtc.cfg.ermsg.noShiftNumber);
 		
-		ttcofdtcResult.shifts[shiftNumber] = [];
+		const notSellsTicketsArray = ticketsList.data.filter(
+			item => item.operation_type !== 'OPERATION_SELL'
+		);
+		if (notSellsTicketsArray.length !== 0) {
+			ttcofdtc.result.unknownTypeTickets[shiftNumber] = notSellsTicketsArray;
+		}	
+
+		ttcofdtc.result.shifts[shiftNumber] = [];
 		
 		for (let metaTicketIndex = 0; metaTicketIndex < filteredTicketsArray.length; metaTicketIndex++) {
-			manager.log(cfg.logTab, cfg.logTab, cfg.logTab, 'Конвертирование чека: ', metaTicketIndex+1, '/', filteredTicketsArray.length);
+			manager.log(ttcofdtc.cfg.logTab, ttcofdtc.cfg.logTab, 'Конвертирование чека: ', metaTicketIndex+1, '/', filteredTicketsArray.length);
 			await manager.handleMetaTicket(filteredTicketsArray[metaTicketIndex], shiftNumber);
 		}
-		manager.log(cfg.logTab, cfg.logTab, 'Пауза: ', cfg.pauses.shift/1000, ' секунд');
-		await manager.pause(cfg.pauses.shift);
+		manager.log(ttcofdtc.cfg.logTab, 'Пауза: ', ttcofdtc.cfg.pauses.shift/1000, ' секунд');
+		await manager.pause(ttcofdtc.cfg.pauses.shift);
 	},
 	async handleMetaTicket(metaTicket, shiftNumber) {
-		validation.assert(metaTicket.id !== undefined, cfg.ermsg.noTicketID);
+		if (ttcofdtc.state.manualThrow) {
+			throw new Error('manualThrow');
+		}
+		validation.assert(metaTicket.id !== undefined, ttcofdtc.cfg.ermsg.noTicketID);
 		const ticketResponse = await fetcher.showTicketByID(
-			metaTicket.id, state.dates.start, state.dates.end,
-			cfg.sec_ch_ua_header, state.referrer
+			metaTicket.id, ttcofdtc.state.dates.start, ttcofdtc.state.dates.end,
+			ttcofdtc.cfg.sec_ch_ua_header, ttcofdtc.state.referrer
 		);
 		const ticket = await ticketResponse.json();
-		ttcofdtcResult.shifts[shiftNumber].push(
+		ttcofdtc.result.shifts[shiftNumber].push(
 			dataWorker.getConvertedTicket(ticket)
 		);
-		manager.log(cfg.logTab, cfg.logTab, cfg.logTab, 'Пауза: ', cfg.pauses.ticket/1000, ' секунд');
-		await manager.pause(cfg.pauses.ticket);
+		manager.log(ttcofdtc.cfg.logTab, ttcofdtc.cfg.logTab, 'Пауза: ', ttcofdtc.cfg.pauses.ticket/1000, ' секунд');
+		await manager.pause(ttcofdtc.cfg.pauses.ticket);
 	},
 };
 
